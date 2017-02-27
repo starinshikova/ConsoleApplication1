ffce3eab89b3d8dc2b0688b1ee87f282208dccb9
Davydova Alexandra <starinshikova@mail.ru>
2017-02-27 21:50:32 +0700

    выделяет линию, следует за ней на поворотах

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -40,7 +40,7 @@ double fill(IplImage* src, CvPoint seed, CvScalar color = CV_RGB(255, 0, 0))
 		0);
 
 	// покажем площадь заливки
-  	printf("[filled area] %.2f\n", comp.area);
+  	//printf("[filled area] %.2f\n", comp.area);
 
 	return comp.area;
 
@@ -55,24 +55,10 @@ inline bool isBlack(int x, uint8_t *row)
 	return ((b*0.0722 + g*0.7152 + r*0.2126) < 60);
 }
 
-/*inline bool isBlack(int x, uint8_t *row)
-{
-	int r = row[3 * x + 2];
-	int g = row[3 * x + 1];
-	int b = row[3 * x + 0];
-	r = r >> 4;
-	r = r << 4;
-	g = g >> 4;
-	g = g << 4;
-	b = b >> 4;
-	b = b << 4;
-	return ((b+ g + r) < 60);
-}*/
-
 int main(int argc, char* argv[])
 {
 	// имя файла задаётся первым параметром
-	char* filename = "video.avi";
+	char* filename = "n14.avi";
 
 	printf("[i] file: %s\n", filename);
 
@@ -84,15 +70,17 @@ int main(int argc, char* argv[])
 
 	f.open("1.txt");
 
-	//Mat edge, gray2, img2, draw;
+
 	int x = argc >= 3 ? atoi(argv[2]) : 200;
 	int y = argc >= 4 ? atoi(argv[3]) : 434;
-	int width = argc >= 5 ? atoi(argv[4]) : 200;
+	int width = argc >= 5 ? atoi(argv[4]) : 300;
 	int height = argc >= 6 ? atoi(argv[5]) : 45;
 	int add = argc >= 7 ? atoi(argv[6]) : 400;
+	double area1=0, area2=0, dif=0, proc=0;
 	Mat orig;
-	int blDotX = 0, blDotX2 = 0, blDotX3 = 0, blDotX4 = 0, Xco = 0, Xco2 = 0, count = 0;
-	uint8_t *row, *row2;
+	int blDotX3 = 0, blDotX4 = 0, Xco = 0, Xco2 = 0, count = 0;
+	uint8_t *row2;
+
 	while (1) {
 		// получаем следующий кадр
 		frame = cvQueryFrame(capture);
@@ -103,51 +91,74 @@ int main(int argc, char* argv[])
 		// здесь можно вставить
 		// процедуру обработки
 		dst = cvCloneImage(frame);
+
+		//set the ROI in copy image (like temp image for finding center of black line)
 		cvSetImageROI(dst, cvRect(x, y, width, height));
-		//cvSetImageROI(frame, cvRect(x, y, width, height));
-		//cvAddS(frame, cvScalar(add), frame);
-		//cvResetImageROI(frame);
+
+		//convert image to matrix
 		orig = cvarrToMat(dst);
-		//imshow("orig", orig);
-		//row = (uint8_t*)orig.ptr<uint8_t>(0);
+
+		//save the row from matrix with x=44
 		row2 = (uint8_t*)orig.ptr<uint8_t>(44);
 		
-		//blDotX = 0;
-		//blDotX2 = 200;
+		//set the starting point of searching black point in bottom line of ROI
 		blDotX3 = 0;
 		blDotX4 = 200;
-		//while (!isBlack(blDotX, row))
-		//{
-		//	blDotX++;
-		//}
-		//while (!isBlack(blDotX2, row))
-		//{
-		//	blDotX2--;
-		//}
+
+		//finding black point from the left (from 0 to ..+)
 		while (!isBlack(blDotX3, row2))
 		{
 			blDotX3++;
 		}
+
+		//finding black point from the right (from 200 to ...-)
 		while (!isBlack(blDotX4, row2))
 		{
 			blDotX4--;
 		}
-		//Xco = (blDotX + blDotX2) / 2;
+
+		// center of black line
 		Xco2 = (blDotX3 + blDotX4) / 2;
-		//Xco = (Xco + Xco2) / 2;
-		printf("[x center] %.2u\n", Xco2);
+
+		//printf("[x center] %.2u\n", Xco2);
 		if (count == 0) {
 			x += (int)Xco2 - 57; count++;
 		}
 
+		//set image ROI
 		cvSetImageROI(frame, cvRect(x + Xco2-width/2, 435, 200, 45));
-     		cvAddS(frame, cvScalar(200), frame);
+
+		//color ROI in blue
+     	cvAddS(frame, cvScalar(200), frame);
+
+		//reset ROI
 		cvResetImageROI(frame);
 
+		//calculate and fill with red area in black line from center of black line
+		area2 = fill(frame, cvPoint(x + Xco2, 479), CV_RGB(250, 0, 0));
 
-		f << fill(frame, cvPoint(x + Xco2, 479), CV_RGB(250, 0, 0))   << sizeof(double) << endl;
-		//fwrite(&fill, sizeof(fill), 1, f); 
+		//if we have first frame of video
+		if (count == 1) {
+			area1=area2; count++;
+		}
 
+		//calculating diffarance
+		dif = fabs(area2 - area1);
+		proc = dif / area2 * 100;
+
+		//write in file f
+		f << dif  << sizeof(double) << endl;
+		f << area1 << sizeof(double) << endl;
+		f << area2 << sizeof(double) << endl;
+		f << proc << sizeof(double) << endl;
+
+		printf("[area1] %.2f\n", area1);
+		printf("[area2] %.2f\n", area2);
+		printf("[differance area] %.2f\n", dif);
+		printf("[differance area in proc] %.2f\n", proc);
+
+		area1 = area2;
+		x = x + Xco2 - width / 2;
 
 		// показываем кадр
 		cvShowImage("original", frame);

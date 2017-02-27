4b8aa00150458654b4d1ecfdeb8647109e4bea19
Davydova Alexandra <starinshikova@mail.ru>
2017-02-27 14:02:49 +0700

    videlyaet chernuu liniu

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -40,7 +40,7 @@ double fill(IplImage* src, CvPoint seed, CvScalar color = CV_RGB(255, 0, 0))
 		0);
 
 	// покажем площадь заливки
-	printf("[filled area] %.2f\n", comp.area);
+  	printf("[filled area] %.2f\n", comp.area);
 
 	return comp.area;
 
@@ -54,6 +54,20 @@ inline bool isBlack(int x, uint8_t *row)
 
 	return ((b*0.0722 + g*0.7152 + r*0.2126) < 60);
 }
+
+/*inline bool isBlack(int x, uint8_t *row)
+{
+	int r = row[3 * x + 2];
+	int g = row[3 * x + 1];
+	int b = row[3 * x + 0];
+	r = r >> 4;
+	r = r << 4;
+	g = g >> 4;
+	g = g << 4;
+	b = b >> 4;
+	b = b << 4;
+	return ((b+ g + r) < 60);
+}*/
 
 int main(int argc, char* argv[])
 {
@@ -70,11 +84,11 @@ int main(int argc, char* argv[])
 
 	f.open("1.txt");
 
-	Mat edge, gray2, img2, draw;
-	int x = argc >= 3 ? atoi(argv[2]) : 240;
-	int y = argc >= 4 ? atoi(argv[3]) : 370;
-	int width = argc >= 5 ? atoi(argv[4]) : 150;
-	int height = argc >= 6 ? atoi(argv[5]) : 130;
+	//Mat edge, gray2, img2, draw;
+	int x = argc >= 3 ? atoi(argv[2]) : 200;
+	int y = argc >= 4 ? atoi(argv[3]) : 434;
+	int width = argc >= 5 ? atoi(argv[4]) : 200;
+	int height = argc >= 6 ? atoi(argv[5]) : 45;
 	int add = argc >= 7 ? atoi(argv[6]) : 400;
 	Mat orig;
 	int blDotX = 0, blDotX2 = 0, blDotX3 = 0, blDotX4 = 0, Xco = 0, Xco2 = 0, count = 0;
@@ -95,20 +109,21 @@ int main(int argc, char* argv[])
 		//cvResetImageROI(frame);
 		orig = cvarrToMat(dst);
 		//imshow("orig", orig);
-		row = (uint8_t*)orig.ptr<uint8_t>(5);
-		row2 = (uint8_t*)orig.ptr<uint8_t>(105);
-		blDotX = 0;
-		blDotX2 = 149;
+		//row = (uint8_t*)orig.ptr<uint8_t>(0);
+		row2 = (uint8_t*)orig.ptr<uint8_t>(44);
+		
+		//blDotX = 0;
+		//blDotX2 = 200;
 		blDotX3 = 0;
-		blDotX4 = 149;
-		while (!isBlack(blDotX, row))
-		{
-			blDotX++;
-		}
-		while (!isBlack(blDotX2, row))
-		{
-			blDotX2--;
-		}
+		blDotX4 = 200;
+		//while (!isBlack(blDotX, row))
+		//{
+		//	blDotX++;
+		//}
+		//while (!isBlack(blDotX2, row))
+		//{
+		//	blDotX2--;
+		//}
 		while (!isBlack(blDotX3, row2))
 		{
 			blDotX3++;
@@ -117,20 +132,20 @@ int main(int argc, char* argv[])
 		{
 			blDotX4--;
 		}
-		Xco = (blDotX + blDotX2) / 2;
+		//Xco = (blDotX + blDotX2) / 2;
 		Xco2 = (blDotX3 + blDotX4) / 2;
-		Xco = (Xco + Xco2) / 2;
-		printf("[x center] %.2u\n", Xco);
+		//Xco = (Xco + Xco2) / 2;
+		printf("[x center] %.2u\n", Xco2);
 		if (count == 0) {
-			x += (int)Xco - 57; count++;
+			x += (int)Xco2 - 57; count++;
 		}
 
-		cvSetImageROI(frame, cvRect(Xco, 435, 210, 45));
-		cvAddS(frame, cvScalar(200), frame);
+		cvSetImageROI(frame, cvRect(x + Xco2-width/2, 435, 200, 45));
+     		cvAddS(frame, cvScalar(200), frame);
 		cvResetImageROI(frame);
 
 
-		f << fill(frame, cvPoint(Xco, 435), CV_RGB(250, 0, 0))   << sizeof(double) << endl;
+		f << fill(frame, cvPoint(x + Xco2, 479), CV_RGB(250, 0, 0))   << sizeof(double) << endl;
 		//fwrite(&fill, sizeof(fill), 1, f); 
 
 
